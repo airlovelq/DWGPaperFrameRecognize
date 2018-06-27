68070a5108122dd42249a7d7815dc4917faf9c19
airlovelq <34851208+airlovelq@users.noreply.github.com>
2018-06-27 13:40:15 +0800

    增加二值化图片后的reshape
    增加二值化图片后的reshape

--- PyDwgPaperFrameRecognize/TrainProcess.py
+++ PyDwgPaperFrameRecognize/TrainProcess.py
@@ -20,6 +20,18 @@ if __name__ == "__main__":
     datatest = np.array(datatest)
     datatrain = datatrain.astype('float32') / 255
     datatest = datatest.astype('float32') / 255
+
+    #因图片数据进行了二值化，需要增加一维度
+    shapetrain = np.shape(datatrain)
+    shapetrainnew = shapetrain + (1,)
+    datatrain = np.reshape(datatrain,shapetrainnew)
+    print(np.shape(datatrain))
+
+    shapetest = np.shape(datatest)
+    shapetestnew = shapetest + (1,)
+    datatest = np.reshape(datatest, shapetestnew)
+    print(np.shape(datatest))
+
     typetest = np.array(typetest)
     typetrain = np.array(typetrain)
     TsDwgFrameNet.train(datatrain, typetrain, datatest, typetest, dlg.strModelPath+'/'+dlg.strModelName)
